3ec22d1c234bc4d41c76c5483e0db7392cae3d32
Olena Rikabal Aviles <OAviles@lan.ad>
2018-02-06 12:39:07 +0200

    done

--- code.js
+++ code.js
@@ -1,53 +1,52 @@
 let url = 'https://www.eliftech.com/school-task';
+let delayReq = 3000; // I've decided to send requests every n seconds, if the "start" button was pressed
 
 const startButton = document.getElementById('start');
 const stopButton = document.getElementById('stop');
 const status = document.getElementById('status');
-const error = document.getElementById('error');
 
+startButton.addEventListener('click', () => { startButton.disabled = true; start(); });
+stopButton.addEventListener('click', () => { location.reload(); });
 
-startButton.addEventListener('click', () => { synchronize(); start() });
-stopButton.addEventListener('click', function() { startButton.disabled = true; });
-
-function synchronize(prev) {
+function synchronize() {
 	status.innerHTML = 'Synchronize...'	
-	loadExpressions(function(data){
-		if (!data) return;
-		if(!!prev && prev.id !== data.id) return;
-		synchronize(data);
-	});
+	setDelay(1000);	
 }
-function start() {
-	let startTime = Date.now();	
+function start() {	
 	loadExpressions(function(data){
 	if (!data) return;
 
 	status.innerHTML = 'Calculating...'
 	let set = data.expressions, expression;
 	let res = [];
-	console.log(set);	
+	
 	for (let i = 0; i < set.length; i++) {
-		expression = set[i].replace("\"", "");
-		console.log(expression);
-		res.push(calculate(expression));
-		console.log(res[i]);
+		expression = set[i].replace("\"", "");		
+		res.push(calculate(expression));		
 	}
 	let postData = {
 		id: data.id,
 		results: res
 	}
-
+	
 	sendAjax(postData, function(result) {
-		status.innerHTML = 'Waiting a 10 sec...';	
-		
-		let div = document.createElement('div');
-		div.innerHTML = result;
-		document.body.appendChild(div);
-	});
-	console.log(postData);
-	let endTime = Date.now();
-	setTimeout(function(){ startButton.click() }, 10000-(endTime -startTime));
+		// the falsy result means that expression is expired, so I'll be waiting a second
+		if (result === false) synchronize(); 		
+		else {
+			// display the result of calculation
+			let div = document.createElement('div');
+			div.innerHTML = 'result: passed';
+			document.body.appendChild(div);			
+			// Waiting a "delayReq" sec...			
+			status.innerHTML = `Waiting a ${delayReq/1000} sec...`;
+			setDelay(delayReq);
+		}				
+	});		
 });
+}
+
+function setDelay(delay) {	
+	setTimeout(()=>{ start(); }, delay);
 }
 
 function loadExpressions(cb) {
@@ -69,11 +68,12 @@ function loadExpressions(cb) {
 	}
 }
 
+// the function calculate the expression in Reverse Polish notation
 function calculate(expression) {
 	let exp = expression.split(" ");
 	let re = /\+|-|\*|\//;
 	let res = [];
-	let j = 0;
+	let j = 0; // the length of the "res" array
 
 	for (let i = 0; i < exp.length; i++) {		
 		if (!exp[i].match(re)) {
@@ -114,5 +114,6 @@ function sendAjax (data, cb) {
 	  }	  
 	  cb(result.passed);
 	};
+	
 	xhr.send(JSON.stringify(data));
   }
